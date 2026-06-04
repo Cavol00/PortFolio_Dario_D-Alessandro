aa35358409d1318d0f919a83c7922a6548f991f4
Cavol00 <dario.dalessandro77@gmail.com>
2026-06-04 19:03:32 +0200

    dio aram

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const progetti = [{
     {
         titolo: "[[ Dashboard di Analisi ]]",
         descrizione: "[[ Pannello di controllo web responsive con grafici dinamici, modalità scura nativa e gestione dei dati in tempo reale. ]]",
-        url: "https://cavol00.github.io/picchio_rosso/ ",
+        url: "https://cavol00.github.io/picchio_rosso/",
         tecnologie: ["React", "Tailwind", "Chart.js"]
     },
     {
